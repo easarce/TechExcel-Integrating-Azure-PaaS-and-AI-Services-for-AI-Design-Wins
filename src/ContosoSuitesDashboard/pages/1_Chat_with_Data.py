169663d8de82bb19e1b2bf3496df787d01c21d04
easarce <63259562+easarce@users.noreply.github.com>
2024-12-04 20:45:47 +0000

    modified secrets file

--- src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
+++ src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
@@ -89,7 +89,7 @@ def main():
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-    # Display chat messages from history on app rerun
+    # Display chat messages from history on app rerun.
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
